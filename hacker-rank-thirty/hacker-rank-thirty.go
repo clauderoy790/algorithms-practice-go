97ec6b82f5fc9c0504c6a0416c0b06a2b031e5bd
Claude Roy <claude.roy790@gmail.com>
2021-07-27 17:38:37 -0500

    solve day eleven

--- hacker-rank-thirty/hacker-rank-thirty.go
+++ hacker-rank-thirty/hacker-rank-thirty.go
@@ -11,7 +11,7 @@ import (
 type HackerRankThirty struct{}
 
 func (hr *HackerRankThirty) Resolve() {
-	dayTen()
+	dayEleven()
 }
 
 func scannerExample() {
@@ -162,4 +162,71 @@ func dayTen() {
 		}
 	}
 	fmt.Println(maxOne)
+}
+const (
+	INT_MAX = int32(^uint32(0) >> 1)
+	INT_MIN = ^INT_MAX
+)
+func dayEleven() {
+	hWidth := 3
+	//matrix := [][]int32{
+	//	{1, 1, 1, 0, 0, 0},
+	//	{0, 1, 0, 0, 0, 0},
+	//	{1, 1, 1, 0, 0, 0},
+	//	{0, 0, 2, 4, 4, 0},
+	//	{0, 0, 0, 2, 0, 0},
+	//	{0, 0, 1, 2, 4, 0},
+	//}
+	matrix := [][]int32{
+		{-1, -1, 0, -9, -2, -2},
+		{-2, -1, -6, -8, -2, -5},
+		{-1, -1, -1, -2, -3, -4},
+		{-1, -9, -2, -4, -4, -5},
+		{-7, -3, -3, -2, -9, -9},
+		{-1, -3, -1, -2, -4, -5},
+	}
+	biggestSum := INT_MIN
+	points := getHourglassPoints(matrix, hWidth)
+
+	for _, p := range points {
+		sum := sumHourglass(matrix, p, hWidth)
+		if sum > biggestSum {
+			biggestSum = sum
+		}
+	}
+	fmt.Println("the biggest sum for the matrix is: ", biggestSum)
+}
+
+func sumHourglass(matrix [][]int32, p Point, width int) (sum int32) {
+	removedPoints := 0
+	pointIncrement := 1
+	for y := 0; y < width; y++ { // Loop the height
+		for x := 0; x < width;x++ {
+			if x >= removedPoints && x < width - removedPoints {
+				sum += matrix[p.y +y][p.x+x]
+			}
+		}
+		if width-removedPoints*2 == 1 { // change direction
+			pointIncrement *= -1
+		}
+		removedPoints += pointIncrement
+	}
+	return
+}
+
+func getHourglassPoints(matrix [][]int32, width int) []Point {
+	var pts []Point
+	matWidth, matHeight := len(matrix[0]), len(matrix)
+	for y := 0; y < matHeight;y++ {
+		for x := 0; x < matWidth;x++ {
+			if x+width-1 < matWidth && y+width-1 < matHeight {
+				pts = append(pts, Point{x, y})
+			}
+		}
+	}
+	return pts
+}
+
+type Point struct {
+	x, y int
 }
